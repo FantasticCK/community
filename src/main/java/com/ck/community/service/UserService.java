790976188503b866668ac57aae867eeaca352a61
FantasticCK <kainingc@usc.edu>
2021-05-25 00:53:23 -0700

    开发登录和退出功能

--- src/main/java/com/ck/community/service/UserService.java
+++ src/main/java/com/ck/community/service/UserService.java
@@ -1,6 +1,8 @@
 package com.ck.community.service;
 
+import com.ck.community.dao.LoginTicketMapper;
 import com.ck.community.dao.UserMapper;
+import com.ck.community.entity.LoginTicket;
 import com.ck.community.entity.User;
 import com.ck.community.util.CommunityConstant;
 import com.ck.community.util.CommunityUtil;
@@ -34,6 +36,9 @@ public class UserService implements CommunityConstant {
 
     @Value("${server.servlet.context-path}")
     private String contextPath;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     public User findUserById(int id) {
         return userMapper.selectById(id);
@@ -105,4 +110,52 @@ public class UserService implements CommunityConstant {
         return ACTIVATION_SUCCESS;
     }
 
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+
+        // 验证账号
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            map.put("usernameMsg", "该账号不存在！");
+            return map;
+        }
+
+        // 验证账号状态
+        if (user.getStatus() == 0) {
+            map.put("usernameMsg", "该账号未激活！");
+            return map;
+        }
+
+        // 验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(password)) {
+            map.put("passwordMsg", "密码不正确！");
+            return map;
+        }
+
+        // 生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000L * expiredSeconds));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
+
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket, 1);
+    }
 }
--- src/main/java/com/ck/community/util/CommunityConstant.java
+++ src/main/java/com/ck/community/util/CommunityConstant.java
@@ -5,7 +5,7 @@ public interface CommunityConstant {
     /**
      * 激活成功
      */
-    int ACTIVATION_SUCCESS =0;
+    int ACTIVATION_SUCCESS = 0;
 
     /**
      * 重复激活
@@ -16,4 +16,14 @@ public interface CommunityConstant {
      * 激活失败
      */
     int ACTIVATION_FAILURE = 2;
+
+    /**
+     * 默认状态的登陆凭证超时时间
+     */
+    int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
+
+    /**
+     * 记住状态下登陆凭证超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 }
