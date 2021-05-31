606977ee9fc1b0003f2bc50822e7fefa6d2fb17c
FantasticCK <kainingc@usc.edu>
2021-05-31 14:36:06 -0700

    实现查看帖子详情功能

--- src/main/java/com/ck/community/controller/DiscussPostController.java
+++ src/main/java/com/ck/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.ck.community.controller;
 import com.ck.community.entity.DiscussPost;
 import com.ck.community.entity.User;
 import com.ck.community.service.DiscussPostService;
+import com.ck.community.service.UserService;
 import com.ck.community.util.CommunityUtil;
 import com.ck.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,9 @@ public class DiscussPostController {
 
     @Autowired
     private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private HostHolder hostHolder;
@@ -40,6 +46,17 @@ public class DiscussPostController {
 
         // 报错的情况, 将来统一处理
         return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 查询帖子
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost);
+        // 查询作者
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user", user);
+        return "/site/discuss-detail";
     }
 
 }
