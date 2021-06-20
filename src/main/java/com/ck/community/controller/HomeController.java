fff09c20677871c7c44ffddb1023116f2974d4de
FantasticCK <kainingc@usc.edu>
2021-06-19 22:38:55 -0700

    实现点赞功能

--- src/main/java/com/ck/community/controller/HomeController.java
+++ src/main/java/com/ck/community/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.ck.community.entity.DiscussPost;
 import com.ck.community.entity.Page;
 import com.ck.community.entity.User;
 import com.ck.community.service.DiscussPostService;
+import com.ck.community.service.LikeService;
 import com.ck.community.service.UserService;
+import com.ck.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,13 +19,16 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page){
@@ -40,6 +45,10 @@ public class HomeController {
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
+
                 discussPosts.add(map);
             }
         }
---
+++ src/main/java/com/ck/community/service/LikeService.java
@@ -0,0 +1,38 @@
+package com.ck.community.service;
+
+import com.ck.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // 点赞
+    public void like(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+        if (isMember) {
+            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+        } else {
+            redisTemplate.opsForSet().add(entityLikeKey, userId);
+        }
+    }
+
+    // 查询某实体点赞的数量
+    public long findEntityLikeCount(int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        return redisTemplate.opsForSet().size(entityLikeKey);
+    }
+
+    // 查询某人对某实体的点赞状态 (有没有点过赞)
+    // 1 - 点过赞, 0 - 没点过
+    public int findEntityLikeStatus(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
+    }
+
+}
